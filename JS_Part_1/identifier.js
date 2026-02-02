244bf020e1cd293ef0d391b20eb4757d8ad53aec
Shashank <shashankesi224@gmail.com>
2026-02-02 10:32:59 +0530

    Add identifier.js - JavaScript identifiers and variable declarations

---
+++ JS_Part_1/identifier.js
@@ -0,0 +1,15 @@
+let price;// valid identifier
+
+let price123; // valid identifier
+
+let _price; // valid identifier
+
+let $price; // valid identifier
+
+// let old price; // not allowed due to space
+
+// let 123price; // not allowed due to starting with a number
+
+let priceNew=5; // valid identifier
+console.log("Price using let: " + priceNew);
+//  console.log("Not valid if we change the case sensitivity of the identifier" +PriceNew);
