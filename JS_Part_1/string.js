9ec5398c957288fd8eb4724517f6ef8dd75ccbb7
Shashank <shashankesi224@gmail.com>
2026-02-13 14:08:44 +0530

    // This is a combination of 2 commits. // The first commit's message is:

---
+++ JS_Part_1/string.js
@@ -0,0 +1,15 @@
+//string 
+let naam="Shashank";
+let role="Developer";
+console.log(naam);
+console.log(typeof naam);
+
+let num="123";
+console.log(num);
+console.log(typeof num);
+
+let empty="";
+console.log(empty);
+console.log(typeof empty);
+
+
