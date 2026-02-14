f0e7a7d515061d317ace149964904bc0e6c40e5d
Shashank <shashankesi224@gmail.com>
2026-02-14 12:29:25 +0530

    // Please enter the commit message for your changes. Lines starting // with '#' will be ignored, and an empty message aborts the commit.

---
+++ JS_Part_1/truthy&falsy.js
@@ -0,0 +1,18 @@
+// truthy
+if (true) {
+    console.log('This is truthy');
+}else {
+    console.log('This is falsy');
+}
+
+if (1) {
+    console.log('This is truthy');
+}else {
+    console.log('This is falsy');
+}
+
+if(0){
+    console.log('This is truthy');
+}else {
+    console.log('This is falsy');
+}
