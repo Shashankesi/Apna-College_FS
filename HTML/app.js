4bfd807375942c72b20ca861992541ec9a27ebc8
Shashank <shashankesi224@gmail.com>
2026-02-25 17:56:43 +0530

    // This is a combination of 2 commits.

--- HTML/app.js
+++ HTML/app.js
@@ -75,3 +75,16 @@ let student3="Aash";
 
 let students=["Aman","Shashank","Aash"];
 console.log(students);
+
+//object literals
+let person = {
+    name: "Shashank",
+    age: 22,
+    hobbies: ["coding", "gaming", "cooking"],
+    isStudent: true,
+    greet: function() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+};
+
+person.greet();
--- javaScript/ObjectLiterals.js
+++ javaScript/ObjectLiterals.js
@@ -8,3 +8,15 @@ let person = {
         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 };
+
+let delhi={
+    name:"Delhi",
+    population: 20000000,
+    famousFor: "Monuments",
+    longitude: 77.1025,
+    latitude: 28.7041,
+    getInfo: function() {
+        console.log(`${this.name} has a population of ${this.population} and is famous for ${this.famousFor}.`);
+    }
+}
+delhi.getInfo();
