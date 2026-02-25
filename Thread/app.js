3af4c2a14857a67fc7f491c2ff3721021cc76f2b
Shashank <shashankesi224@gmail.com>
2026-02-25 18:11:53 +0530

    // This is a commit message template. You can edit this message to describe the changes you have made in this commit.

---
+++ Thread/app.js
@@ -0,0 +1,7 @@
+const posts={
+    username:"Shashank",
+    content:"Hello World",
+    likes:100,
+    repost:50,
+    tags:["#hello","#world"] 
+}
---
+++ javaScript/getValues.js
@@ -0,0 +1,10 @@
+let student={
+    name:"Shashank",
+    age:22,
+    course:"MCA",
+    hobbies:["coding","gaming","traveling"]
+};
+console.log(student.name); // Accessing name
+console.log(student.age); // Accessing age
+console.log(student.course); // Accessing course
+console.log(student.hobbies); // Accessing hobbies
