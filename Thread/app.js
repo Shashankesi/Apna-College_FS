b73a6c8581a15549b4e68b5ce58fa28c1a5c0e5a
Shashank <shashankesi224@gmail.com>
2026-04-16 17:04:22 +0530

    multiple JsS files have been edited and one file has been added

--- Thread/app.js
+++ Thread/app.js
@@ -5,3 +5,21 @@ const posts={
     repost:50,
     tags:["#hello","#world"] 
 }
+
+// get values of the posts object
+console.log(posts.username); // this will print "Shashank"
+console.log(posts.content); // this will print "Hello World"
+console.log(posts.likes); // this will print 100
+console.log(posts.repost); // this will print 50
+console.log(posts.tags); // this will print ["#hello","#world"] 
+
+let student={
+    name:"Shashank",
+    age:20,
+    grade:"A",
+    subjects:["Math","Science","English"]
+}
+console.log(student.name); // this will print "Shashank"
+console.log(student.age); // this will print 20
+console.log(student.grade); // this will print "A"
+console.log(student.subjects); // this will print ["Math","Science","English"]
---
+++ javaScript/randomInteger.js
@@ -0,0 +1,8 @@
+let num= Math.random(); // this will generate a random number between 0 and 1
+console.log(num); 
+num=num*10;
+console.log(num); 
+num=Math.floor(num); // this will round the number down to the nearest integer
+console.log(num);
+num=num+1; // this will add 1 to the number to get a number between 1 and 10
+console.log(num); // this will print a random integer between 1 and 10
