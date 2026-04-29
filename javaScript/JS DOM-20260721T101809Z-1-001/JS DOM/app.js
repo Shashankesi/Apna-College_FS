ebe09af46d85e87f4565c6e13fefec53b39c851d
Shashankesi <shashankesi224@gmail.com>
2026-04-29 12:00:00 +0530

    Add JS DOM inspection script

---
+++ javaScript/JS DOM-20260721T101809Z-1-001/JS DOM/app.js
@@ -0,0 +1,9 @@
+let smallImg=document.getElementsByClassName("oldImg");
+for(let i=0;i<smallImg.length;i++){
+    console.dir(smallImg[i]);
+}
+
+let tagName=document.getElementsByTagName("p");
+for(let i=0;i<tagName.length;i++){
+    console.dir(tagName[i]);
+}
