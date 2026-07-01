f1cc428ce920e8ea855beea35762ce8fb83de870
Shashankesi <shashankesi224@gmail.com>
2026-07-01 22:09:24 +0530

    chnage made in default parameter value in subtr function

--- javaScript/array/default.js
+++ javaScript/array/default.js
@@ -5,3 +5,9 @@ function add(a,b=3){
 }
  console.log('add(2):', add(2)); //5
  console.log('add(2,4):', add(2,4)); //6
+
+function subtr(a,b=3){
+    return a-b;
+}
+ console.log('subtr(5):', subtr(5)); //2
+ console.log('subtr(5,2):', subtr(5,2)); //3
