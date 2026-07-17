2f2681fad0c7133d96214a5570fc2826931ba0f3
Shashankesi <shashankesi224@gmail.com>
2026-07-17 18:46:08 +0530

    spread with array literals code added

--- javaScript/array/spread.js
+++ javaScript/array/spread.js
@@ -2,3 +2,14 @@ let arr=[1,2,3,4,5];
 console.log(Math.min(...arr)); //1
 
 console.log(...arr); //1 2 3 4 5
+
+
+// spread with array literal
+
+let arr1=[1,2,3,4,5];
+
+let newArr=[...arr1];
+console.log(newArr); //[1,2,3,4,5]
+
+let newArr1=[...arr1,6,7,8];
+console.log(newArr1); //[1,2,3,4,5,6,7,8]
